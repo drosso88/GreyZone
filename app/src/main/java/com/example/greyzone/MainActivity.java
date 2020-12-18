cf4f58a8cb2e78906861b7b182bc932e8f1b2b8c
rocio <rocio.soriano.gomez@gmail.com>
2020-12-18 12:25:44 +0100

    con el formulario de nuevo usuario finalizado

--- app/src/main/java/com/example/greyzone/MainActivity.java
+++ app/src/main/java/com/example/greyzone/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.greyzone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    TextView btnRegistrate;
+    EditText user, pass;
+    Button login;
+
+//Esta clase se encarga del Inicio de sesión
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//declaramos las variables y las igualamos con los xml
+
+        user = findViewById(R.id.Nick);
+        pass = findViewById(R.id.Password);
+
+        login = findViewById(R.id.inicioSesion);
+        btnRegistrate=findViewById(R.id.Registro);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario, password;
+
+                usuario = user.getText().toString();
+                password = pass.getText().toString();
+
+                //comparamos strings y avisamos si algo queda en blanco con un TOAST (mensaje emergente)
+                 if (user.equals("")) {
+                    Toast.makeText(MainActivity.this, "El Usuario está en blanco", Toast.LENGTH_LONG).show();
+                }
+                else if (pass.equals("")) {
+                    Toast.makeText(MainActivity.this, "La contraseña está en blanco", Toast.LENGTH_LONG).show();
+                }else{
+                    //nos autenticamos si todo va bien
+            }
+
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // lo ponemos para que vya al formulario de registro para ver si funciona de momento.
+                // Esto hay que cambiarlo luego
+                Intent i =new Intent(MainActivity.this,Registro.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        //le damos funcionalidad del boton al texto, aun no funciona correctamente, pero lo hará
+        btnRegistrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(MainActivity.this,Registro.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    };
     }
-}
+
--- app/src/main/java/com/example/greyzone/Registro.java
+++ app/src/main/java/com/example/greyzone/Registro.java
@@ -1,13 +1,92 @@
 package com.example.greyzone;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
+    EditText Nick, Password, Dni, Telefono, Nombre, Apellidos, Email;
+    Button registro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Nick = findViewById(R.id.Nick);
+        Password = findViewById(R.id.Password);
+        Dni = findViewById(R.id.Dni);
+        Email = findViewById(R.id.Email);
+        Telefono = findViewById(R.id.Telefono);
+        Nombre = findViewById(R.id.Nombre);
+        Apellidos = findViewById(R.id.Apellidos);
+        registro = findViewById(R.id.Registro);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nick, password, dni, telefono, nombre, apellidos, email;
+
+                nick = Nick.getText().toString();
+                password = Password.getText().toString();
+                dni = Dni.getText().toString();
+                telefono = Telefono.getText().toString();
+                nombre = Nombre.getText().toString();
+                apellidos = Apellidos.getText().toString();
+                email = Email.getText().toString();
+
+                //ponemos avisos para no dejar ningun campo en blanco
+
+                if (nick.equals("")) ;
+                {
+                    Toast.makeText(Registro.this, "El Usuario está en blanco", Toast.LENGTH_LONG).show();
+                }
+                if (password.equals("")) ;
+                {
+                    Toast.makeText(Registro.this, "La contraseña está en blanco", Toast.LENGTH_LONG).show();
+                }
+                if (dni.equals("")) ;
+                {
+                    Toast.makeText(Registro.this, "El DNI está en blanco", Toast.LENGTH_LONG).show();
+                }
+                if (Telefono.equals("")) ;
+                {
+                    Toast.makeText(Registro.this, "El teléfono está en blanco", Toast.LENGTH_LONG).show();
+                }
+                if (Nombre.equals("")) ;
+                {
+                    Toast.makeText(Registro.this, "El nombre está en blanco", Toast.LENGTH_LONG).show();
+                }
+                if (Apellidos.equals("")) ;
+                {
+                    Toast.makeText(Registro.this, "Los apellidos están en blanco", Toast.LENGTH_LONG).show();
+                }
+                if (email.equals("")) ;
+                {
+                    Toast.makeText(Registro.this, "El email está en blanco", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent R = new Intent(Registro.this,MainActivity.class);
+                startActivity(R);
+                finish();
+            }
+        });
     }
 }
+
+
+
+
+
+
